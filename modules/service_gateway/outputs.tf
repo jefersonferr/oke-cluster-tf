03a0020404e5a6bec83d757eb403b0a0dcd38cca
jeferson.ferreira <jefersonferr@gmail.com>
2026-03-06 23:49:39 -0300

    Bug fix outputs.tf

--- modules/service_gateway/outputs.tf
+++ modules/service_gateway/outputs.tf
@@ -5,5 +5,5 @@ output "service_gateway_display_name" {
     value = oci_core_service_gateway.service_gateway.display_name
 }
 output "all_region_services" {
-  value = data.oci_core_services.all_services.services[0].name
+  value = data.oci_core_services.all_services.services[0].cidr_block
 }
